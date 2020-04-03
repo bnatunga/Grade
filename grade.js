0eade16b5b6f694a801c00e204f6a264ee817b75
apolaydiana <apolatdiana@gmail.com>
2020-04-03 11:55:53 +0300

    first commit

---
+++ grade.js
@@ -0,0 +1,17 @@
+let grade =55;
+grade=50
+
+if(grade>=80 && grade<=100){
+    console.log("You got an A")
+} else
+if(grade>=65 && grade<80){
+    console.log("You got a B")
+} else
+if(grade>=50 && grade<65){
+    console.log("You passed")
+} else
+if(grade<50 && grade>=0){
+    console.log("You failed the test")
+}else{
+    console.log("Grade must be between 0 and 100")
+    }
